7076bb9db8e9c9d7e70931cb52d27f5736ddfcce
Tara Haseltine <tara.haseltine@gmail.com>
2017-05-11 17:09:47 -0400

    added message area to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description, :picture, :location, :tag, :price, :user_id)
+      params.require(:post).permit(:title, :description, :picture, :location, :tag, :price, :user_id, :phone_number)
     end
 end
