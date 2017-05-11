234471fb79ffdafd32e31f83da03953602615431
probablynotfredcobb <fredcobbthe3rd@gmail.com>
2017-05-11 16:09:50 -0400

    still working on User creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :are_you_new?
 
-  private
+
   def are_you_new?
     this_id = request.session_options[:id]
-    User.find_or_create_by(session_id: this_id)
+    @current_user = User.find_or_create_by(session_id: this_id)
   end
 end
