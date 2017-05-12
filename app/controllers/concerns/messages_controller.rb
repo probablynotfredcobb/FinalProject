0f07695d9506b174e6e972fcc1a80f729b4a9c84
probablynotfredcobb <fredcobbthe3rd@gmail.com>
2017-05-12 13:33:22 -0400

    Message sender backend

--- app/controllers/concerns/messages_controller.rb
+++ app/controllers/concerns/messages_controller.rb
@@ -1,17 +1,19 @@
+class MessagesController < ApplicationController
+  def create
+    MessageSender.send_message(message)
+    redirect_to root_url,
+      success: 'Thank you! The poster should be contacting you shortly.'
+  rescue Twilio::REST::RequestError => error
+    p error.message
+    redirect_to root_url,
+      error: 'Oops! There was an error. Please try again.'
+  end
 
-require 'twilio-ruby'
+  private
 
-#Account info
-
-@client = Twilio::REST::Client.new account_sid, auth_token
-
-
-
-
-@client.account.messages.create({
-  :from => '+13016846938',
-  :to => @post_number,
-  :body => "Hi this person is interested in your post #{@post_title}. Contact them at #{@phone_number}. #{@message}"
-  # ,
-  # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
-})
+  def message
+    "New lead received for #{params[:house_title]}. " \
+    "Call #{params[:name]} at #{params[:phone]}. " \
+    "Message: #{params[:message]}"
+  end
+end
