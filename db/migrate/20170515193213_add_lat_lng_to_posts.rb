23d666bd992812bf8dada6f51650a92093c6347e
probablynotfredcobb <fredcobbthe3rd@gmail.com>
2017-05-15 17:26:20 -0400

    added post location

---
+++ db/migrate/20170515193213_add_lat_lng_to_posts.rb
@@ -0,0 +1,7 @@
+class AddLatLngToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :posts, :lat, :string
+    add_column :posts, :lng, :string
+    remove_column :posts, :avatar 
+  end
+end
