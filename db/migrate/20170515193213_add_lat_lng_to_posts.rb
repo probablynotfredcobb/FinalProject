7df0ac5dec36506c7752053903b93eb4225526ff
Tara Haseltine <tara.haseltine@gmail.com>
2017-05-22 16:39:58 -0400

    commented out column deletion

--- db/migrate/20170515193213_add_lat_lng_to_posts.rb
+++ db/migrate/20170515193213_add_lat_lng_to_posts.rb
@@ -2,6 +2,6 @@ class AddLatLngToPosts < ActiveRecord::Migration[5.1]
   def change
     add_column :posts, :lat, :string
     add_column :posts, :lng, :string
-    remove_column :posts, :avatar 
+    # remove_column :posts, :avatar 
   end
 end
